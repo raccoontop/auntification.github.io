ff0be3e26e1de3e955f9dc2a482b0fd42fefacde
raccoontop <145014099+raccoontop@users.noreply.github.com>
2024-12-08 21:03:35 +0100

    Create ProtectedController.java

---
+++ ProtectedController.java
@@ -0,0 +1,13 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ProtectedController {
+
+    @GetMapping("/protected")
+    public String getProtectedResource() {
+        return "You have accessed a protected resource!";
+    }
+}
